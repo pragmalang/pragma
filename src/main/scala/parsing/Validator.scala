6d95d3e69c91261147380a11ebd6fd10ad409388
Tabzz98 <muhammadtabaza@gmail.com>
2019-09-29 21:19:39 +0300

    Added field default value validation.

--- src/main/scala/parsing/Validator.scala
+++ src/main/scala/parsing/Validator.scala
@@ -2,10 +2,17 @@ package parsing
 import domain._
 import scala.util._
 
-object Validator {
-  def apply(constructs: List[HConstruct]): Try[List[HConstruct]] = {
-    Success(Nil)
+class Validator(val st: List[HConstruct]) {
+  def checkFieldValueType: Try[Unit] = Try {
+    for {
+      construct <- st
+      if construct.isInstanceOf[HModel]
+    } for {
+      field <- construct.asInstanceOf[HModel].fields
+      default <- field.defaultValue
+    } if (field.htype != default.htype)
+      throw new Exception(
+        s"Invalid default value of type ${default.htype} for field of type ${field.htype}"
+      )
   }
-
-  def checkFieldValueType(constructs: List[HConstruct]) = Nil
 }
