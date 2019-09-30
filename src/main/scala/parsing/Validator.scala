0df61f16e9592d11b8cad7db2d346f73775be453
Tabzz98 <muhammadtabaza@gmail.com>
2019-09-30 17:11:11 +0300

    Added identity validation

--- src/main/scala/parsing/Validator.scala
+++ src/main/scala/parsing/Validator.scala
@@ -1,5 +1,6 @@
 package parsing
 import domain._
+import utils._
 import scala.util._
 
 class Validator(val st: List[HConstruct]) {
@@ -11,8 +12,25 @@ class Validator(val st: List[HConstruct]) {
       field <- construct.asInstanceOf[HModel].fields
       default <- field.defaultValue
     } if (field.htype != default.htype)
-      throw new Exception(
-        s"Invalid default value of type ${default.htype} for field of type ${field.htype}"
+      throw new PositionedUserError(
+        s"Invalid default value of type ${default.htype} for field of type ${field.htype}",
+        field.position.get
       )
+  }
+
+  def checkIdentity: Try[Unit] = Try {
+    st.foldLeft(Set.empty[String])(
+      (ids, construct) =>
+        construct match {
+          case i: Identifiable with Positioned =>
+            if (ids(i.id))
+              throw new PositionedUserError(
+                s"`${i.id}` is defined twice",
+                i.position.get
+              )
+            else ids + i.id
+          case _ => ids
+        }
+    )
   }
 }
