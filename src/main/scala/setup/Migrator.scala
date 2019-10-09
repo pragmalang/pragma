aaa7e6516e8c6d6368c71041b2126d6874be7c12
Anas Albarghouthy <anasbar97@gmail.com>
2019-10-09 13:05:04 +0300

    - Create `MockSuccessMigrator` - Create `MockFailureMigrator`

--- src/main/scala/setup/Migrator.scala
+++ src/main/scala/setup/Migrator.scala
@@ -1,6 +1,6 @@
 package setup
 
-import scala.util.{Try}
+import scala.util.{Try, Success, Failure}
 import sangria.schema.{Schema}
 import sangria.renderer.{SchemaRenderer, SchemaFilter}
 
@@ -11,4 +11,12 @@ trait Migrator {
 case object PrismaMigrator extends Migrator {
   def apply(schema: Schema[Any, Any]) =
     ??? // TODO: use SchemaFilter when rendering the schema
+}
+
+case object MockSuccessMigrator extends Migrator {
+  def apply(schema: Schema[Any,Any]): Try[Unit] = Success(())
+}
+
+case object MockFailureMigrator extends Migrator {
+  def apply(schema: Schema[Any,Any]): Try[Unit] = Failure(new Exception("Mock Migrator failed"))
 }
