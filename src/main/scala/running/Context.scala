434f7030efb878b58ed0724fcb6dbdecf7eecc81
Anas Albarghouthy <anasbar97@gmail.com>
2019-10-30 00:38:30 +0200

    Edit `Request` and `Context`

--- src/main/scala/running/Context.scala
+++ src/main/scala/running/Context.scala
@@ -4,10 +4,5 @@ import sangria.ast.Document
 
 case class Context(
     data: Option[JsValue],
-    graphQlQuery: Document,
-    body: Option[JsObject],
-    cookies: Map[String, String],
-    url: String,
-    hostname: String,
     user: Option[JwtPaylod]
 )
--- src/main/scala/running/Request.scala
+++ src/main/scala/running/Request.scala
@@ -1,3 +1,12 @@
 package running
+import spray.json.{JsObject, JsString, JsValue}
+import sangria.ast.Document
 
-case class Request(ctx: Context)
+case class Request(
+  ctx: Context,
+  graphQlQuery: Document,
+  body: Option[JsObject],
+  cookies: Map[String, String],
+  url: String,
+  hostname: String,
+)
