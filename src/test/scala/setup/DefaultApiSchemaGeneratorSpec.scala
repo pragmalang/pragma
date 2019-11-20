3d7cd6d07dd1214b92a1f10d23f53d3a49372da2
Anas Albarghouthy <anasbar97@gmail.com>
2019-11-20 22:47:48 +0200

    Fix `buildApiSchema` method test

--- src/test/scala/setup/DefaultApiSchemaGeneratorSpec.scala
+++ src/test/scala/setup/DefaultApiSchemaGeneratorSpec.scala
@@ -249,17 +249,173 @@ class DefaultApiSchemaGeneratorSpec extends FunSuite {
 
   test("buildApiSchema method works") {
     val expected = gql"""
+    type Query {
+      business(email: String!): Business!
+      branch(address: String!): Branch!
+      manyBusinesses(where: WhereInput): [Business]!
+      branches(where: WhereInput): [Branch]!
+      countManyBusinesses(where: WhereInput): Int!
+      countBranches(where: WhereInput): Int!
+      businessExists(filter: LogicalFilterInput!): Int!
+      branchExists(filter: LogicalFilterInput!): Int!
+    }
+    
+    type Mutation {
+      createBusiness(business: BusinessObjectInput!): Business!
+      createBranch(branch: BranchObjectInput!): Branch!
+      updateBusiness(email: String!, business: BusinessOptionalInput!): Business!
+      updateBranch(address: String!, branch: BranchOptionalInput!): Branch!
+      upsertBusiness(business: BusinessOptionalInput!): Business!
+      upsertBranch(branch: BranchOptionalInput!): Branch!
+      deleteBusiness(email: String!): Business!
+      deleteBranch(address: String!): Branch!
+      createManyBusinesses(manyBusinesses: [BusinessObjectInput]!): [Business]!
+      createBranches(branches: [BranchObjectInput]!): [Branch]!
+      updateManyBusinesses(manyBusinesses: [BusinessReferenceInput]!): [Business]!
+      updateBranches(branches: [BranchReferenceInput]!): [Branch]!
+      upsertManyBusinesses(manyBusinesses: [BusinessOptionalInput]!): [Business]!
+      upsertBranches(branches: [BranchOptionalInput]!): [Branch]!
+      deleteManyBusinesses(email: [String]!): [Business]!
+      deleteBranches(address: [String]!): [Branch]!
+    }
+    
     type Subscription {
       business(email: String, on: [SingleRecordEvent]): BusinessNotification!
       branch(address: String, on: [SingleRecordEvent]): BranchNotification!
       manyBusinesses(where: WhereInput, on: [MultiRecordEvent]): [BusinessNotification]!
       branches(where: WhereInput, on: [MultiRecordEvent]): [BranchNotification]!
     }
+    
+    input EqInput {
+      field: String!
+      value: Any!
+    }
+    
+    input WhereInput {
+      filter: LogicalFilterInput
+      orderBy: OrderByInput
+      range: RangeInput
+      first: Int
+      last: Int
+      skip: Int
+    }
+    
+    input OrderByInput {
+      field: String!
+      order: OrderEnum
+    }
+    
+    enum OrderEnum {
+      DESC
+      ASC
+    }
+    
+    input RangeInput {
+      before: ID!
+      after: ID!
+    }
+    
+    input LogicalFilterInput {
+      AND: [LogicalFilterInput]
+      OR: [LogicalFilterInput]
+      predicate: FilterInput
+    }
+    
+    input FilterInput {
+      eq: EqInput
+    }
+    
+    enum MultiRecordEvent {
+      CREATE
+      UPDATE
+      READ
+      DELETE
+    }
+    
+    enum SingleRecordEvent {
+      UPDATE
+      READ
+      DELETE
+    }
+    
+    scalar Any
+    
+    type Business {
+      username: String
+      email: String!
+      password: String!
+      branches(where: WhereInput): [Branch]!
+      mainBranch: Branch
+      businessType: BusinessType!
+    }
+    
+    type Branch {
+      address: String!
+      business: Business!
+    }
+    
+    enum BusinessType {
+      FOOD
+      CLOTHING
+      OTHER
+    }
+    
+    input BusinessObjectInput {
+      username: String
+      email: String!
+      password: String!
+      branches: [BranchReferenceInput]!
+      mainBranch: BranchReferenceInput
+      businessType: BusinessType!
+    }
+    
+    input BranchObjectInput {
+      address: String!
+      business: BusinessReferenceInput!
+    }
+    
+    input BusinessReferenceInput {
+      username: String
+      email: String!
+      password: String
+      branches: [BranchReferenceInput]
+      mainBranch: BranchReferenceInput
+      businessType: BusinessType
+    }
+    
+    input BranchReferenceInput {
+      address: String!
+      business: BusinessReferenceInput
+    }
+    
+    input BusinessOptionalInput {
+      username: String
+      email: String
+      password: String
+      branches: [BranchOptionalInput]
+      mainBranch: BranchOptionalInput
+      businessType: BusinessType
+    }
+    
+    input BranchOptionalInput {
+      address: String
+      business: BusinessOptionalInput
+    }
+    
+    type BusinessNotification {
+      event: MultiRecordEvent!
+      business: Business!
+    }
+    
+    type BranchNotification {
+      event: MultiRecordEvent!
+      branch: Branch!
+    }
     """.renderPretty
 
     val schema =
       generator.buildApiSchema.renderPretty
 
-    println(schema)
+    assert(schema == expected)
   }
 }
