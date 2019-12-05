592760401d454de83502a977295d62917907a276
Anas Albarghouthy <anasbar97@gmail.com>
2019-12-05 06:01:05 +0200

    Modify `Storage` trait

--- src/main/scala/setup/storage/Storage.scala
+++ src/main/scala/setup/storage/Storage.scala
@@ -1,23 +1,25 @@
 package setup.storage
 import sangria.ast.Document
 import domain.SyntaxTree
-import running.Request
 import scala.util.Try
 import setup._, setup.utils._
+import spray.json.JsValue
 
 trait Storage {
   val syntaxTree: SyntaxTree
   val migrator: Migrator
 
-  def runQuery(query: Document): Request
-  def validateQuery(query: Document): Try[Request]
+  def runQuery(query: Document): JsValue
+  def validateQuery(query: Document): Try[JsValue]
+  def reduceQuery(query: Document): Document
   def migrate() = migrator.run()
   def dockerComposeYaml(): DockerCompose
 }
 
 case class PrismaMongo(syntaxTree: SyntaxTree) extends Storage {
   override val migrator: Migrator = PrismaMongoMigrator(syntaxTree)
-  override def runQuery(query: Document): Request = ???
-  override def validateQuery(query: Document): Try[Request] = ???
+  override def runQuery(query: Document): JsValue = ???
+  override def validateQuery(query: Document): Try[JsValue] = ???
   override def dockerComposeYaml() = ???
+  override def reduceQuery(query: Document): Document = ???
 }
