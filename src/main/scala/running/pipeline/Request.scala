8abe604d056c82484c495d4203f15bea46c6bb2e
Anas Albarghouthy <anasbar97@gmail.com>
2019-12-18 02:19:25 +0200

    Remove unnecessary functions

--- src/main/scala/running/pipeline/Request.scala
+++ src/main/scala/running/pipeline/Request.scala
@@ -13,43 +13,4 @@ case class Request(
     body: Option[JsObject],
     syntaxTree: Option[SyntaxTree] = None,
 ) extends PipelineInput
-    with PipelineOutput {
-  lazy val resource: Try[List[Resource]] = Try {
-    val opDef = ctx.query.definitions
-      .find(_.isInstanceOf[OperationDefinition])
-      .get
-      .asInstanceOf[OperationDefinition]
-    val selectedModels = syntaxTree.get.models.filter { model =>
-      opDef.selections.exists({
-        case s: Field => s.name.toLowerCase == model.id.toLowerCase
-        case _        => false
-      })
-    }
-
-    val selectedFields = ???
-    ???
-  }
-}
-
-case class Resource(
-    operation: HEvent,
-    model: HModel,
-    selectedFields: List[SelectedField]
-) {
-  lazy val fieldsHooks =
-    selectedFields.map(_.field).map(f => f.id -> f.hooks).toMap
-}
-
-case class SelectedField(
-    field: HModelField,
-    selectedFields: List[SelectedField]
-) {
-  lazy val canHaveSelection = field.htype match {
-    case HReference(_) => true
-    case HSelf(_)      => true
-    case _: HShape     => true
-    case _             => false
-  }
-
-  lazy val fieldType = field.htype
-}
+    with PipelineOutput
