1a08e693b18a7ed97b59b8e8566010ff982115c9
Anas Albarghouthy <anas@heavenlyx.com>
2020-10-27 16:52:05 +0300

    Fix logo problem in docs

--- docs/src/main.js
+++ docs/src/main.js
@@ -19,8 +19,8 @@ const purpleLogo = 'http://' + window.location.host + '/src/purple-logo.svg'
 function addLinkToMenuTitle() {
   const menuBar = document.getElementById("menu-bar");
   const a = document.createElement("a");
-  const title = menuBar.children.item(1).innerText
-  const newMenuTitle = menuBar.children.item(1).cloneNode()
+  const title = menuBar.children.item(0).children.item(1).innerText
+  const newMenuTitle = menuBar.children.item(0).children.item(1).cloneNode()
   newMenuTitle.innerText = title
 
   const isLightTheme = () => {
@@ -63,8 +63,8 @@ function addLinkToMenuTitle() {
   a.appendChild(logoImage);
   a.setAttribute("href", "https://pragmalang.com")
   a.className = "menu-title custom-a"
-  menuBar.children.item(1).insertAdjacentElement("afterend", a)
-  menuBar.children.item(1).remove()
+  menuBar.children.item(0).children.item(1).insertAdjacentElement("afterend", a)
+  menuBar.children.item(0).children.item(1).remove()
 }
 
 newTabExternalLinks();
