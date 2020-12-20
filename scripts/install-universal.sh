d2bfe53b03cf6a1a928ef74531ac95f80ed7c6b6
Tabzz98 <muhammadtabaza@gmail.com>
2020-12-20 03:55:16 +0200

    Modified Linux installation path

--- scripts/install-universal.sh
+++ scripts/install-universal.sh
@@ -16,11 +16,11 @@ unzip -d /tmp/pragma /tmp/pragma.zip
 
 PRAGMA_DIR_NAME=$(ls /tmp/pragma)
 
-echo "Installing Pragma in /usr/lib/$PRAGMA_DIR_NAME..."
+echo "Installing Pragma in /usr/local/lib/$PRAGMA_DIR_NAME..."
 sudo chmod +x /tmp/pragma/$PRAGMA_DIR_NAME/bin/pragma
-sudo mv /tmp/pragma/$PRAGMA_DIR_NAME/ /usr/lib/
+sudo mv /tmp/pragma/$PRAGMA_DIR_NAME/ /usr/local/lib/
 
-echo "Creating symbolic link /usr/lib/$PRAGMA_DIR_NAME/bin/pragma -> /usr/bin/pragma"
-sudo ln -s /usr/lib/$PRAGMA_DIR_NAME/bin/pragma /usr/bin/pragma
+echo "Creating symbolic link /usr/local/lib/$PRAGMA_DIR_NAME/bin/pragma -> /usr/local/bin/pragma"
+sudo ln -s /usr/local/lib/$PRAGMA_DIR_NAME/bin/pragma /usr/local/bin/pragma
 
 echo 'Pragma has been successfully installed! Run `pragma help` to see what you can do.'
