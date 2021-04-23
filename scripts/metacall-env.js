dd93ea26a55fa5ca4ef458b22b3ea71a79388571
Anas Al-Barghouthy <anasbar97@gmail.com>
2021-04-24 02:20:31 +0300

    Export MetaCall ENV vars inside GitHub action test VM

---
+++ scripts/metacall-env.js
@@ -0,0 +1,14 @@
+/** Run from within the Metacall REPL to get these values */
+function main() {
+    console.log(`export LOADER_SCRIPT_PATH='${process.env.LOADER_SCRIPT_PATH}' \\`)
+    console.log(`export LOADER_LIBRARY_PATH='${process.env.LOADER_LIBRARY_PATH}' \\`)
+    console.log(`export LD_LIBRARY_PATH='${process.env.LOADER_LIBRARY_PATH}' \\`)
+    console.log(`export CONFIGURATION_PATH='${process.env.CONFIGURATION_PATH}' \\`)
+    console.log(`export SERIAL_LIBRARY_PATH='${process.env.SERIAL_LIBRARY_PATH}' \\`)
+    console.log(`export DETOUR_LIBRARY_PATH='${process.env.DETOUR_LIBRARY_PATH}' \\`)
+    console.log(`export PORT_LIBRARY_PATH='${process.env.PORT_LIBRARY_PATH}'`)
+}
+
+main()
+
+module.exports = { main }
