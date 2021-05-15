a382403d7ac58ddb679e05666ccc878ce0d35ffa
Anas Al-Barghouthy <anasbar97@gmail.com>
2021-05-16 01:46:46 +0300

    Fix Unix universal installation script to support reinstallation

--- scripts/install-universal.sh
+++ scripts/install-universal.sh
@@ -6,7 +6,10 @@ set -e
 
 DOWNLOAD_URL='https://github.com/pragmalang/pragma/releases/download/1.0.0RC/pragma.zip'
 
-sudo rm -rf /usr/local/bin/pragma /tmp/pragma.zip
+sudo rm -rf /usr/local/bin/pragma
+sudo rm -rf /usr/local/lib/pragma
+sudo rm -rf /tmp/pragma.zip 
+sudo rm -rf /tmp/pragma
 
 echo 'Downloading Pragma CLI...'
 curl -H 'Cache-Control: no-cache' -Lo /tmp/pragma.zip $DOWNLOAD_URL
@@ -14,13 +17,11 @@ curl -H 'Cache-Control: no-cache' -Lo /tmp/pragma.zip $DOWNLOAD_URL
 echo 'Extracting files...'
 unzip -d /tmp/pragma /tmp/pragma.zip
 
-PRAGMA_DIR_NAME=$(ls /tmp/pragma)
+echo "Installing Pragma in /usr/local/lib/pragma..."
+sudo chmod +x /tmp/pragma/pragma/pragma
+sudo mv /tmp/pragma/pragma/ /usr/local/lib/
 
-echo "Installing Pragma in /usr/local/lib/$PRAGMA_DIR_NAME..."
-sudo chmod +x /tmp/pragma/$PRAGMA_DIR_NAME/pragma
-sudo mv /tmp/pragma/$PRAGMA_DIR_NAME/ /usr/local/lib/
-
-echo "Creating symbolic link /usr/local/lib/$PRAGMA_DIR_NAME/bin/pragma -> /usr/local/bin/pragma"
-sudo ln -s /usr/local/lib/$PRAGMA_DIR_NAME/pragma /usr/local/bin/pragma
+echo "Creating symbolic link /usr/local/lib/pragma/bin/pragma -> /usr/local/bin/pragma"
+sudo ln -s /usr/local/lib/pragma/pragma /usr/local/bin/pragma
 
 echo 'Pragma has been successfully installed! Run `pragma help` to see what you can do.'
